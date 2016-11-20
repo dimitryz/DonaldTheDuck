fd9d77d1c521bf32455c8d045c7043cf011545a3
Dimitry Zolotaryov <dimitry@webit.ca>
2016-11-20 11:25:25 -0500

    Added initial replacement

---
+++ chrome/content.js
@@ -0,0 +1,20 @@
+/// Function iterates over all children of el and replaces instances of Donald Trump with Donald Duck
+var replaceText = function (node) {
+    if (node.nodeName == "#text") {
+        if (node.nodeValue.match(/Trump/)) {
+            node.nodeValue = node.nodeValue.replace(/Trump/, "Duck");
+        }
+    } else {
+        for (var i = 0; i < node.childNodes.length; i++) {
+            replaceText(node.childNodes[i]);
+        }
+    }
+}
+
+/// Triggers the pattern replacement when document is ready
+var tid = setInterval( function () {
+    if ( document.readyState !== 'complete' ) return;
+    clearInterval( tid );       
+    
+    replaceText(document);
+}, 100 );
