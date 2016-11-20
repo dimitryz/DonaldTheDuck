bffda5cb9c900656307ab48881056f0363ff2627
Dimitry Zolotaryov <dimitry@webit.ca>
2016-11-20 11:52:23 -0500

    Added replacing of content after having modified the DOM

--- chrome/content.js
+++ chrome/content.js
@@ -2,19 +2,43 @@
 var replaceText = function (node) {
     if (node.nodeName == "#text") {
         if (node.nodeValue.match(/Trump/)) {
-            node.nodeValue = node.nodeValue.replace(/Trump/, "Duck");
+            node.nodeValue = node.nodeValue.replace(/(J\.\s+)?Trump/, "Duck");
         }
     } else {
         for (var i = 0; i < node.childNodes.length; i++) {
             replaceText(node.childNodes[i]);
         }
+        // node.childNodes.forEach(function (n) {
+        //     replaceText(n);
+        // });
     }
+}
+
+/**
+ * Sets up a monitor to check for changes
+ * 
+ * @param DOMNode A node to monitor
+ * @return MutationObserver The observer
+ */
+var monitorForChanges = function (node) {
+    var observer = new MutationObserver(function(mutations) {
+        for (var i = 0; i < mutations.length; i++) {
+            for (var j = 0; j < mutations[i].addedNodes.length; j++) {
+                replaceText(mutations[i].addedNodes[j]);
+            }
+        }
+    });
+    observer.observe(node, { attributes: true, childList: true, characterData: true, subtree: true });
+    return observer;
 }
 
 /// Triggers the pattern replacement when document is ready
 var tid = setInterval(function () {
     if (document.readyState !== 'complete') return;
-    clearInterval(tid);       
+    clearInterval(tid);    
+
+    console.log("Donald Duck initialized.");   
     
     replaceText(document);
+    monitorForChanges(document.body);
 }, 100);
